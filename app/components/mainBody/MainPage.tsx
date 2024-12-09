0b51814867ffd81579a53fd63f5ca432649c5e7f
Parmendra-Panwar <parmendrapanwar11@gmail.com>
2024-12-09 13:51:13 +0530

    Fixing UI ..

--- app/components/mainBody/MainPage.tsx
+++ app/components/mainBody/MainPage.tsx
@@ -1,3 +1,37 @@
+// "use client";
+// import { useState } from "react";
+// import styling from "./MainPage.module.css";
+// import TopSection from "./TopSection";
+// import FilterSection from "./FilterSection";
+// import CardSection from "./CardSection";
+
+// const MainPage: React.FC = () => {
+//   const [filtersVisible, setFiltersVisible] = useState(true);
+
+//   const toggleFilters = () => {
+//     setFiltersVisible((prev) => !prev);
+//   };
+
+//   return (
+//     <div className={styling.GrendContainer}>
+//       <TopSection
+//         itemCount={3425}
+//         onFilterToggle={toggleFilters}
+//         filtersVisible={filtersVisible}
+//       />
+
+//       <div className={styling.mainContainer}>
+//         <div className={filtersVisible ? "" : styling.hidden}>
+//           <FilterSection />
+//         </div>
+//         <CardSection />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default MainPage;
+
 "use client";
 import { useState } from "react";
 import styling from "./MainPage.module.css";
@@ -21,10 +55,17 @@ const MainPage: React.FC = () => {
       />
 
       <div className={styling.mainContainer}>
-        <div className={filtersVisible ? "" : styling.hidden}>
+        {/* Conditionally render the FilterSection */}
+        <div
+          className={`${styling.FilterSection} ${
+            filtersVisible ? "" : styling.hidden
+          }`}
+        >
           <FilterSection />
         </div>
-        <CardSection />
+        <div className={styling.CardSection}>
+          <CardSection />
+        </div>
       </div>
     </div>
   );
